b9cb7973525008eb9a70e814a154cd00b234591d
Jose Biescas <jose.biescas@upr.edu>
2020-04-14 13:48:20 -0400

    added PLY and base Lexer

---
+++ lexer.py
@@ -0,0 +1,86 @@
+import ply.lex as lex
+from ply.lex import TOKEN
+
+class Lexer(object):
+
+    keywords = {
+        'for' : 'FOR',
+        'at' : 'AT',
+        'while' : 'WHILE',
+        'stop' : 'STOP',
+        'next' : 'NEXT',
+    }
+    literals = [
+        '+', '-', '/', '*', '^', '(', ')', '[',
+     ']', '{', '}', '|', '&', '?', '=', '<', '>'
+     ]
+
+    tokens = list(keywords.values()) + [
+
+        #Identifier
+        'ID',
+
+        #Operators (Can't be decalred in literals)
+        'LTOE', 'GTOE',
+
+        #Digits
+        'INT', 'FLOAT',
+
+        #Booleans
+        'TRUE', 'FALSE',
+    ]
+
+    #RULES
+    t_LTOE = r'<='
+    t_GTOE = r'>='
+    t_ignore = ' \t'
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += 1
+        pass
+
+    def t_ID(self, t):
+        r'[a-zA-Z][a-zA-Z0-9]*'
+        t.type = self.keywords.get(t.value, t.type)
+        return t
+
+    def t_TRUE(self,t):
+        r'true'
+        t.value = True
+        return t
+
+    def t_FALSE(self, t):
+        r'false'
+        t.value = False
+        return t
+
+    def t_FLOAT(self, t):
+        r'[+-]?([0-9]*[.])?[0-9]+'
+        t.value = float(t.value)
+        return t
+
+    def t_INT(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
+
+    def t_error(self, t):
+        print("Illegal character %s" % t.value[0])
+        t.lexer.skip(1)
+
+    # Build the lexer
+    def build(self,**kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+
+    def test(self,data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok: 
+                break
+            print(tok)
+
+m = Lexer()
+m.build()
+#m.test() Uncomment to test lexer, must pass in a string
