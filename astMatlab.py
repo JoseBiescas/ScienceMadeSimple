78811ca53ed6e4a41a8c590fad45937d47475e26
Jose Biescas <jose.biescas@upr.edu>
2020-05-01 13:08:56 -0400

    added classes for binop and unary

---
+++ astMatlab.py
@@ -0,0 +1,73 @@
+import operator
+from types import LambdaType
+
+class MAIN:
+    def __init__(self, childs=None):
+        if childs is None:
+            childs = []
+        self.childs = childs
+    
+    def __len__(self):
+        return len(self.childs)
+    
+    def __repr__(self):
+        return 'Main {0}'.format(self.childs)
+
+    def __iter__(self):
+        return iter(self.childs)
+    
+    def evaluate(self):
+        returnlist = []
+        for n in self:
+            result = n.evaluate()
+
+            if result is not None:
+                returnlist.append(result)
+        
+        return returnlist
+
+class BaseExpression:
+    def evaluate(self):
+        raise NotImplementedError()
+
+class BINOP(BaseExpression):
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '%': operator.mod,
+        '>': operator.gt,
+        '>=': operator.ge,
+        '<': operator.lt,
+        '<=': operator.le,
+        '^': operator.pow
+    }
+    def __init__(self, operation, num1, num2):
+        self.operation = operation
+        self.num1 = num1
+        self.num2 = num2
+
+    def __repr__(self):
+        return 'BinOP : {0} {1} {2}'.format(self.num1, self.operation, self.num2)
+
+    def evaluate(self):
+        op = self.operations[self.operation]
+        num1 = self.num1.evaluate()
+        num2 = self.num2.evaluate()
+
+        return op(num1, num2)
+
+class UNARY(BaseExpression):
+    operators = {
+        '-' : operator.neg
+    }
+    def __init__(self, operation, expression: BaseExpression):
+        self.operation = operation
+        self.expression = expression
+    
+    def __repr__(self):
+        return 'UNARY : {0} {1}'.format(self.operation, self.expression)
+    
+    def evaluate(self):
+        return self.operators[self.operation](self.expression.evaluate())
--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import ply.yacc as yacc
+import astMatlab as ast
 import math
 
 from lexer import Lexer
@@ -12,48 +13,42 @@ class Parser():
     precedence = (
         ('left', '+', '-'),
         ('left', '*', '/'),
+        ('left', '^'),
         ('right', 'UMINUS'),
     )
 
     def p_program(self, p):
         '''program : expression'''
+        p[0] = ast.MAIN([p[1]])
 
-        p[0] = p[1]
-
+    # Done this way so there is precedence clearly stated
     def p_expression_binop(self, p):
-        '''expression : expression BINOP expression'''
-        
-        if(p[2] == '+'):
-            p[0] = p[1] + p[3]
-        elif(p[2] == '-'):
-            p[0] = p[1] - p[3]
-        elif(p[2] == '*'):
-            p[0] = p[1] - p[3]
-        elif(p[2] == '/'):
-            p[0] = p[1] / p[3]
-        elif(p[2] == '>'):
-            p[0] = p[1] > p[3]
-        elif(p[2] == '<'):
-            p[0] = p[1] < p[3]
-        elif(p[2] == '>='):
-            p[0] = p[1] >= p[3]
-        elif(p[2] == '<='):
-            p[0] = p[1] <= p[3]
+        '''expression : expression '+' expression %prec '+'
+            | expression '-' expression %prec '-'
+            | expression '*' expression %prec '*'
+            | expression '/' expression %prec '/'
+            | expression '^' expression %prec '^' '''
+        p[0] = ast.BINOP(p[2], p[1], p[3])
 
-    def p_expression_par(self, p):
-        '''expression : '(' expression ')' '''
+    def p_expression_unary(self, p):
+        '''expression : '-' expression %prec UMINUS'''
+        p[0] = ast.UNARY(p[1], p[2])
 
-    def p_expression_assign(self, p):
-        '''expression : ID '=' expression'''
+    # def p_expression_par(self, p):
+    #     '''expression : '(' expression ')' '''
+
+    # def p_expression_assign(self, p):
+    #     '''expression : identifier '=' expression'''
 
     # def p_expression_for_loop(self, p):
     #     '''expression : FOR IDList AT List'''
 
-    def p_expression_ques(self, p):
-        '''expression : expression ',' expression '?' expression'''
+    # def p_expression_ques(self, p):
+    #     '''expression : expression ',' expression '?' expression'''
 
     def p_expression(self, p):
         '''expression : TERM'''
+        p[0] = p[1]
 
     def p_term(self, p):
         '''
@@ -61,20 +56,21 @@ class Parser():
              | empty
              | BOOLEAN
              | ID
-             | '-' NUMBER %prec UMINUS
              | sciences
              '''
+        if(len(p) == 2):
+            p[0] = p[1]
 
     def p_IDList(self, p):
         '''
-        IDList : ID
-               | ID ',' IDList
+        IDList : identifier
+               | identifier ',' IDList
         '''
 
-    # def p_IDENTIFIER(self, p):
-    #     '''
-    #     identifier : ID
-    #     '''
+    def p_IDENTIFIER(self, p):
+        '''
+        identifier : ID
+        '''
 
     def p_empty(self, p):
         'empty :'
@@ -124,7 +120,7 @@ class Parser():
         '''
 
         if(p[1] == 'epercent'):
-            p[0] = (abs(p[3]- p[5])/p[5]) * 100
+            p[0] = (abs(p[3] - p[5])/p[5]) * 100
         else:
             p[0] = p[1]
 
@@ -145,40 +141,40 @@ class Parser():
     # FORMULA = 0.5 * a * t^2 + v0*t + initial position
 
         if (p[1] == 'position'):
-            p[0] =  0.5 * p[3] * p[5]**2 + p[7]*p[5] + p[9]
+            p[0] = 0.5 * p[3] * p[5]**2 + p[7]*p[5] + p[9]
 
     # INITIAL VELOCITY PARAMETERS: final velocity, acceleration, displacement
     # FORMULA: sqrt(vF^2 - 2*a*displacement)
 
         elif (p[1] == 'initial' and p[2] == 'velocity'):
-            p[0] = math.sqrt(p[4]**2 - 2 *  p[6] * p[8])
+            p[0] = math.sqrt(p[4]**2 - 2 * p[6] * p[8])
 
     # FINAL VELOCITY:  initial velocity, acceleration, (displacement or time), boolean
     # FORMULA 1: sqrt(v0^2 + 2*a*displacement), if boolean is true
     # FORMULA 2: v0 + a*t, if boolean is false
 
         elif (p[1] == 'final' and p[2] == 'velocity'):
-            if(p[10]==True):
-                p[0] = math.sqrt(p[4]**2 + 2 *  p[6] * p[8])
+            if(p[10] == True):
+                p[0] = math.sqrt(p[4]**2 + 2 * p[6] * p[8])
             else:
                 p[0] = p[4] + p[6] * p[8]
 
     # AVERAGE VELOCITY PARAMETERS: initial position, final position, time
     # FORMULA = (xF - x0)/t
-        
+
         elif (p[1] == 'average' and p[2] == 'velocity'):
             p[0] = (p[4]-p[6])/p[8]
 
     # VELOCITY IN X PARAMETERS: the overall velocity
     # FORMULA: v*cos(theta)
 
-        elif (p[1]=='velocity' and p[2] == 'in' and p[3] == 'xaxis'):
+        elif (p[1] == 'velocity' and p[2] == 'in' and p[3] == 'xaxis'):
             p[0] == p[5] * math.cos(p[7])
 
     # VELOCITY IN Y PARAMETERS: the overall velocity
     # FORMULA: v*sin(theta)
 
-        elif (p[1]=='velocity' and p[2] == 'in' and p[3] == 'yaxis'):
+        elif (p[1] == 'velocity' and p[2] == 'in' and p[3] == 'yaxis'):
             p[0] == p[5] * math.sin(p[7])
 
     # ACCELERATION PARAMETERS: initial velocity, final velocity, time
@@ -190,13 +186,13 @@ class Parser():
     # POTENTIAL ENERGY PARAMETERS: mass, vertical height
     # FORMULA: m * G * h
 
-        elif (p[1]=='potential' and p[2] == 'energy'):
+        elif (p[1] == 'potential' and p[2] == 'energy'):
             p[0] == p[4] * 9.80665 * p[6]
 
     # KINETIC ENERGY PARAMETERS: mass, velocity
     # FORMULA: 0.5 * m * v^2
 
-        elif (p[1]=='kinetic' and p[2] == 'energy'):
+        elif (p[1] == 'kinetic' and p[2] == 'energy'):
             p[0] == 0.5 * p[4] * p[6]
 
     def p_CHEMISTRY(self, p):
@@ -236,11 +232,11 @@ class Parser():
                 break
             if not s:
                 continue
-            self.parser.parse(s)
+            self.parser.parse(s, debug=True)
             print("Parse finished.")
 
 
 p = Parser()
 p.build()
-p.test_str() #Uncomment for quick testing
+p.test_str()  # Uncomment for quick testing
 # p.test_doc() <- uncomment for testing
